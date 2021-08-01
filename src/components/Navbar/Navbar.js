0ac4ab03cbb6774687a4457ddc65c638b71ec74a
Govind R Sekhar <56358584+NemesisXOX@users.noreply.github.com>
2021-08-01 23:31:13 +0530

    Nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import "./Navbar.css";
 
 export const Navbar = () => {
 	const Nav = styled.div`
@@ -9,16 +10,20 @@ export const Navbar = () => {
 			background: #DA0037;
 			filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 			display:flex;
+			justify-content:space-between;
 			align-items:center;
-			gap:2em;
+			gap:20px;
+			padding:0px 50px;
 		}
 		&
 	`;
 	return (
 		<Nav className="Navbar">
 			<Link to="/"> Home </Link>
-			<Link to="/signin"> Sign In </Link>
-			<Link to="/register"> Register </Link>
+			<div className="signin-signup">
+				<Link className="element" to="/signin"> Sign In </Link>
+				<Link className="element" to="/register"> Register </Link>
+			</div>
 		</Nav>
 	);
 };
