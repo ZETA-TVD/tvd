0fcf2aaa4258d3d2069f4697d8425c830e60e870
Govind R Sekhar <56358584+NemesisXOX@users.noreply.github.com>
2021-08-02 00:40:13 +0530

    frontpage

--- src/pages/Landingpage/Landingpage.js
+++ src/pages/Landingpage/Landingpage.js
@@ -1,11 +1,26 @@
 import React from "react";
-
-
+import "./Frontpage.stysle.scss";
+import Typewriter from 'typewriter-effect';
 
 export const Landingpage = () => {
 	return (
-		<div>
-			
-		</div>
-	);
+		<div className="container">
+			<div className="Main-text">
+				<p>
+				Keeping track of balances <br/>& shared expenses
+				<Typewriter
+					options={{
+						strings: ['with friends', 'with your loved ones','on trips'],
+						autoStart: true,
+						loop: true,
+						delay: 90,
+						deleteSpeed: 150,
+					}}
+				/>
+				</p>
+				
+			</div>
+			<div className="Image"></div>
+    	</div>
+	)
 };
