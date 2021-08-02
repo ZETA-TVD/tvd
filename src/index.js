792d0b5fbb690246f7b76b358b1b7d74f0bf59b1
Kuliscene <adhvaith9@gmail.com>
2021-08-03 00:34:52 +0530

    asksa;k

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import  'tachyons'
+import background from "./assets/pattern.svg";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<div style={{ backgroundImage: `url(${background})` }}>
+				<App />
+			</div>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
 
 
 reportWebVitals();
