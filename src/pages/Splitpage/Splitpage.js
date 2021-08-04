c3f9d4ffddbea63e5123169a6c83576388293053
Kuliscene <adhvaith9@gmail.com>
2021-08-04 22:53:48 +0530

    code copy paste

--- src/pages/Splitpage/Splitpage.js
+++ src/pages/Splitpage/Splitpage.js
@@ -1,31 +1,44 @@
 import React from "react";
 import "./Splitpage.css";
 import { GroupCard } from "../../components/GroupCard/GroupCard.js";
-import {Carousel} from '3d-react-carousal';
+import "react-responsive-carousel/lib/styles/carousel.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
 export class Splitpage extends React.Component{
-    render(){
- const slides= [
-                            <div className="flex">
-                                <GroupCard name={"Tharvadu"}/>
-                                <GroupCard name={"kuli"}/>
-                                <GroupCard name={"sasti"}/>
-                            </div>,
-                            <div className="flex">
-                                <GroupCard name={"vantony"}/>
-                                <GroupCard name={"govi"}/>
-                                <GroupCard name={"suni"}/>
-                            </div>];
+    render() {
         return (
         <div className="container1">
                 <div className="groups">
                         <div className="heading">
                             <h1> Groups</h1>
                         </div>  
-                        <Carousel slides={slides} autoplay={false} interval={1000}/>
+                        <Carousel>
+                                <div className="car">
+                                        <GroupCard name="Tharvadu"/>
+                                        <GroupCard name="Tharvadu"/>
+                                        <GroupCard name="Tharvadu"/>
+                                </div>
+                                 <div className="car">
+                                    <GroupCard name="tvd"/>
+                                    <GroupCard name="tvd"/>
+                                    <GroupCard name="tvd"/>
+                                </div>
+                                 <div className="car">
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                </div>
+                                 <div className="car">
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                </div>
+                         </Carousel>
                 </div>
-                
-        
+
+                <div className="friends">
+
+                </div>
         </div>
     );
 }
