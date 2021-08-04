b5986b77602f9a115d818f52dea4844ead30245c
Kuliscene <adhvaith9@gmail.com>
2021-08-05 00:30:14 +0530

    friends section

--- src/pages/Splitpage/Splitpage.js
+++ src/pages/Splitpage/Splitpage.js
@@ -3,6 +3,8 @@ import "./Splitpage.css";
 import { GroupCard } from "../../components/GroupCard/GroupCard.js";
 import "react-responsive-carousel/lib/styles/carousel.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import { Woman1 } from "../../assets/woman1.jpg";
+import { FriendCard } from "../../components/FriendCard/FriendCard.js";
 
 export class Splitpage extends React.Component{
     render() {
@@ -14,9 +16,9 @@ export class Splitpage extends React.Component{
                         </div>  
                         <Carousel>
                                 <div className="car">
-                                        <GroupCard name="Tharvadu"/>
-                                        <GroupCard name="Tharvadu"/>
-                                        <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
+                                    <GroupCard name="Tharvadu"/>
                                 </div>
                                  <div className="car">
                                     <GroupCard name="tvd"/>
@@ -33,11 +35,20 @@ export class Splitpage extends React.Component{
                                     <GroupCard name="Tharvadu"/>
                                     <GroupCard name="Tharvadu"/>
                                 </div>
-                         </Carousel>
+                        </Carousel>
                 </div>
 
                 <div className="friends">
-
+                    <div className="heading">
+                        <h1>Friends</h1>
+                    </div>
+                    <div className="mainfriends">
+                        <FriendCard name="Sasti Randi" amt="Rs.10" pic=""/>
+                        <FriendCard name="Daddy Kul" amt="Rs.1000" pic=""/>
+                        <FriendCard name="Member Anto" amt="Rs.101" pic=""/>
+                        <FriendCard name="Gobi Manchurian" amt="Rs.19" pic=""/>
+                        <FriendCard name="Sunapila" amt="Rs.27" pic=""/>
+                    </div>
                 </div>
         </div>
     );
