bc335e903de01e212a921f62557d508375ba431a
Antonymous0 <antonyjosephkajk@gmail.com>
2021-08-07 00:17:43 +0530

    pha

--- src/components/NewGroup/NewGroup.js
+++ src/components/NewGroup/NewGroup.js
@@ -2,68 +2,69 @@ import React from "react";
 import './NewGroup.css'
 
 export class NewGroup extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-       formValues: [{ name: "", email : "" }]
-     };
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-  
-  handleChange(i, e) {
-    let formValues = this.state.formValues;
-    formValues[i][e.target.name] = e.target.value;
-    this.setState({ formValues });
-  }
+	constructor(props) {
+		super(props)
+		this.state = { 
+			 formValues: [{ groupname: "", friendname : "" }]
+		 };
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	
+	handleChange(i, e) {
+		let formValues = this.state.formValues;
+		formValues[i][e.target.name] = e.target.value;
+		this.setState({ formValues });
+	}
 
-  addFormFields() {
-    this.setState(({
-      formValues: [...this.state.formValues, { name: "", email: "" }]
-    }))
-  }
+	addFormFields() {
+		this.setState(({
+			formValues: [...this.state.formValues, { friendname: ""}]
+		}))
+	}
 
-  removeFormFields(i) {
-    let formValues = this.state.formValues;
-    formValues.splice(i, 1);
-    this.setState({ formValues });
-  }
+	removeFormFields(i) {
+		let formValues = this.state.formValues;
+		formValues.splice(i, 1);
+		this.setState({ formValues });
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-    alert(JSON.stringify(this.state.formValues));
-  }
+	handleSubmit(event) {
+		event.preventDefault();
+		alert(JSON.stringify(this.state.formValues));
+	}
 
-  render() {
+	render() {
 
-    return (
-        <form  onSubmit={this.handleSubmit}>
-          {this.state.formValues.map((element, index) => (
-            <div className="form-inline" key={index}>
-              <label>Enter Group Name</label>
-              <input type="text" name="name" value={element.name || ""} onChange={e => this.handleChange(index, e)} />
-              <label>Enter Name</label>
-              <input type="text" name="email" value={element.email || ""} onChange={e => this.handleChange(index, e)} />
-              {
-                index ? 
-                  <button type="button"  className="button remove" onClick={() => this.removeFormFields(index)}>Remove</button> 
-                : null
-              }
-            </div>
-          ))}
-          <div className="button-section">
-              <button className="button add" type="button" onClick={() => this.addFormFields()}>Add</button>
-              <button className="button submit" type="submit">Submit</button>
-          </div>
-      </form>
-    );
-  }
+		return (
+				<form  onSubmit={this.handleSubmit}>
+					{this.state.formValues.map((element, index) => (
+						<div className="form-inline" key={index}>
+							{ index ?
+							<div>
+								<label>Enter Friend Name</label>
+								<input type="text" name="friendname" value={element.friendname || ""} onChange={e => this.handleChange(index, e)} />
+							</div>
+							:
+							<div>
+								
+									<label>Enter Group Name</label>
+									<input type="text" name="groupname" value={element.groupname || ""} onChange={e => this.handleChange(index, e)} />
+																
+									<label>Enter Friend Name</label>
+									<input type="text" name="friendname" value={element.friendname || ""} onChange={e => this.handleChange(index, e)} />
+							</div>}
+							{
+								index ? 
+									<button type="button"  className="button remove" onClick={() => this.removeFormFields(index)}>Remove</button> 
+								: null
+							}
+						</div>
+					))}
+					<div className="button-section">
+							<button className="button add" type="button" onClick={() => this.addFormFields()}>Add</button>
+							<button className="button submit" type="submit">Submit</button>
+					</div>
+			</form>
+		);
+	}
 }
-
-     // <div className="box"> 
-     // 	<form>
-     // 		<input type='text' className="boxbox" placeholder="Group Name"/>
-     // 		<input type='text' className="boxbox" placeholder="Enter Name"/>
-     // 		<input type='button' className="boxbox" value="Remove"/>
-     // 	</form>
-     // </div>
-     	
